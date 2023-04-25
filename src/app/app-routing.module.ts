b809090834b0732a662ad2e4ade5109d83435f18
Jacob Timlin <jacobtimlin23@gmail.com>
2023-04-25 16:15:42 -0500

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './game';
+import { LeaderboardComponent } from './leaderboard';
+import { TicTacToeComponent } from './tic-tac-toe';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GameComponent},
+  { path: 'leaderboard', component: LeaderboardComponent },
+  { path: 'tictactoe', component: TicTacToeComponent},
+  //default redirect
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
