acfc263e629f7d347fc4abf098205a1501186386
isexe <isaac.sexe@gmail.com>
2023-04-25 17:50:09 -0500

    Added Table and Grid-List to imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
+// Base Angular Imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Routing Import
 import { AppRoutingModule } from './app-routing.module';
+
+// Component Imports
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { HeaderComponent } from './header/header.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+
+// Angular Imports
 import { FlexLayoutModule } from '@angular/flex-layout';
+
+// Angular Material Imports
+import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -22,9 +32,11 @@ import {MatIconModule} from '@angular/material/icon';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule,
     FlexLayoutModule,
-    MatIconModule
+    MatToolbarModule,
+    MatIconModule,
+    MatTableModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
