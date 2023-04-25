1078276488d5298af4b56c5e2af5bdd750e81e1f
isexe <isaac.sexe@gmail.com>
2023-04-25 18:17:13 -0500

    Started Leaderboard Table

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'game-leaderboard',
   templateUrl: './leaderboard.component.html',
-  styleUrls: ['./leaderboard.component.css']
+  styleUrls: ['./leaderboard.component.css'],
 })
 export class LeaderboardComponent {
+  tableColumns: string[] = ['username', 'wins', 'losses'];
 
+  records = [
+    { username: 'Player1', wins: 1, losses: 0 },
+    { username: 'Player2', wins: 1, losses: 1 },
+    { username: 'Player3', wins: 0, losses: 1 },
+  ];
 }
